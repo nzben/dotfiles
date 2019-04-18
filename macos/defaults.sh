bb50b51167c245310f1169477bf09da72957fc43
Ben Gracewood <ben@vendhq.com>
2019-04-18 21:12:24 +1200

    Update defaults.sh
    Remove key repeats

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -93,7 +93,7 @@ defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 # Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+# defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
